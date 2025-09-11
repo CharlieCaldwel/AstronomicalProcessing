9fae6162fb063e983f5f9c8e3872ad6dc75581f1
CharlieCaldwel <charliecaldwellwa@gmail.com>
2025-09-11 14:51:46 +0800

    Changed alot

--- AstronomicalProcessing.sln/AstronomicalProcessing/MainWindow.xaml.cs
+++ AstronomicalProcessing.sln/AstronomicalProcessing/MainWindow.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System.Text;
+﻿// Charlie Caldwell, CharliesTeam, Sprint 1
+// 11/09/2025
+// Version: 1
+// Astronomical Processing
+// 
+// Inputs, Processes, Outputs
+
+
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,14 +25,18 @@ namespace AstronomicalProcessing
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private readonly Regex _int = new(@"^-?\d+$");
+        bool Sorted = false;
         public MainWindow()
         {
             InitializeComponent();
+           
         }
 
         private void BtnRandClick_Click(object sender, RoutedEventArgs e)
         {
-            if(AstroDataLBX.Items.Count >= 24)
+            if(AstroDataLBX.Items.Count >= 23)
             {
                 AstroDataLBX.Items.Clear();
             }
@@ -35,6 +48,7 @@ namespace AstronomicalProcessing
                     AstroDataLBX.Items.Add(random.Next(10, 91));
                 }
                 StatusMessage("Added Element");
+                Sorted = false;
             }
             else
             {
@@ -45,11 +59,81 @@ namespace AstronomicalProcessing
 
         private void BtnSort_Click(object sender, RoutedEventArgs e)
         {
+            if (AstroDataLBX.Items.Count != 0)
+            {
+                for (int i = 0; i < AstroDataLBX.Items.Count; i++)
+                {
 
+                    for (int j = 0; j < AstroDataLBX.Items.Count; j++)
+                    {
+                        int num1 = int.Parse(AstroDataLBX.Items[i].ToString());
+                        int num2 = int.Parse(AstroDataLBX.Items[j].ToString());
+
+                        if (num1 < num2)
+                        {
+                            AstroDataLBX.Items[i] = num2;
+                            AstroDataLBX.Items[j] = num1;
+                        }
+                    }
+                }
+                Sorted = true;
+                StatusMessage("Sorted Elements");
+            }
         }// end sort
 
         private void BtnSearch_Click(object sender, RoutedEventArgs e)
         {
+            if(SearchBox.Text.Length > 0 && Sorted == true && _int.IsMatch(SearchBox.Text)){ 
+            int min = 0;
+            int max = AstroDataLBX.Items.Count;
+            int mid = (min + max) / 2;
+
+                while (min != max || int.Parse(AstroDataLBX.Items[mid].ToString()) != int.Parse(SearchBox.Text))
+                {
+                    if (int.Parse(SearchBox.Text) > int.Parse(AstroDataLBX.Items[mid].ToString()))
+                    {
+                        min = mid;
+                        mid = (min + max) / 2;
+                    }
+
+                    else if (int.Parse(SearchBox.Text) < int.Parse(AstroDataLBX.Items[mid].ToString()))
+                    {
+                        max = mid;
+                        mid = (min + max) / 2;
+                    }
+
+                    if (int.Parse(AstroDataLBX.Items[mid].ToString()) == int.Parse(SearchBox.Text))
+                    {
+                        AstroDataLBX.SelectedIndex = mid;
+                        MessageBox.Show("Item found", "Found", MessageBoxButton.OK, MessageBoxImage.Information);
+                        AstroDataLBX.ScrollIntoView(AstroDataLBX.SelectedIndex);
+                        break;
+                    }
+                    else if (min == max && int.Parse(AstroDataLBX.Items[mid].ToString()) != int.Parse(SearchBox.Text))
+                    {
+                        MessageBox.Show("Item not found", "Not found", MessageBoxButton.OK, MessageBoxImage.Information);
+                        break;
+                    }
+
+                    if((min + max) / 2 == min)
+                    {
+                        max = min;
+                    }
+                }
+            }
+            else if (AstroDataLBX.Items.Count <= 0)
+            {
+                MessageBox.Show("Please input data into the list before searching", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else if(Sorted == false)
+            {
+                MessageBox.Show("Please sort the list before searching", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else if(!_int.IsMatch(SearchBox.Text))
+            {
+                MessageBox.Show("Please enter a valid integer to search for", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
 
         }// end search
 
@@ -71,19 +155,58 @@ namespace AstronomicalProcessing
                     int selectedIndex = AstroDataLBX.SelectedIndex;
                     int.TryParse(InputBox.Text, out int value);
                     AstroDataLBX.Items.RemoveAt(selectedIndex);
-                    AstroDataLBX.Items[selectedIndex] = value;
+                    AstroDataLBX.Items.Add(value);
                     StatusMessage("Replaced Element");
                 }
                 else
                 {
                     MessageBox.Show("Input isnt a integer, Please enter a number", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
+                Sorted = false;
+            }
+            else if(AstroDataLBX.Items.Count == 0)
+            {
+                MessageBox.Show("Please input data into the list before modifying", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else if (InputBox.Text.Length > 0)
+            {
+                MessageBox.Show("Please input a integer to modify", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else if (AstroDataLBX.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a element to modify", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }// end input
 
         public void StatusMessage(string message)
         {
             StatusTxt.Text = $"Status: {message}";
+        }
+
+        private void TextGotFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            SearchFieldtxt.Text = "";
+        }
+
+        private void TextNotFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (SearchBox.Text.Length == 0)
+            {
+                SearchFieldtxt.Text = "Enter search data";
+            }
+        }
+
+        private void InputGotFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            InputFieldtxt.Text = "";
+        }
+
+        private void InputNotFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (SearchBox.Text.Length == 0)
+            {
+                InputFieldtxt.Text = "Enter data to input";
+            }
         }
     }
 }
